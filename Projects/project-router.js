f6cffa2affbffc63151b934156c508b1be576407
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-03-25 14:44:53 -0700

    git commit -m

--- Projects/project-router.js
+++ Projects/project-router.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const shortid = require("shortid");
 
 router.use(express.json());
 
-router.get("/", (req, res) => {
-    res.status(200).json("Working!");
+let projects = [
+    {
+        id: shortid.generate(),
+        "title": "Paint House",
+        "description": "Get the house painted!"
+    },
+    {
+        id: shortid.generate(),
+        "title": "Keep Savvy",
+        "description": "Open Source Project"
+    }
+];
+
+router.get("/", async (req, res) => {
+try {
+    res.status(200).json(projects);
+    } catch(error) {
+        console.error(error)
+    }
+});
+
+router.post("/", async (req, res) => {
+   try {
+       if(req.body.title && req.body.description) {
+           const freshProject = {
+               id: shortid.generate(),
+               title: req.body.title,
+               description: req.body.description
+           };
+
+           projects.push(freshProject);
+
+           res.status(201).send(`You have successfully added a new project name ${req.body.title}`);
+       }
+   } catch (error) {
+
+   }
 });
 
 module.exports = router;
