5ef80e8803a6cf9c706c19806c1ac10056134c50
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-03-25 14:11:15 -0700

    Added a route path to the home of the API

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const server = require("./server");
 
 const port = process.env.PORT;
 // make the server listen to requests
+
+server.get('/', (req, res) => {
+    res.send('Welcome to Survani API');
+});
+
 server.listen(port, () => {
     console.log(`Server running at: http://localhost:${port}/`);
 });
